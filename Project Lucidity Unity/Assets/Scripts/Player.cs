c35c59f02a77dc9812d9073d0ec114c329cf54a5
bonfire203 <46801720+bonfire203@users.noreply.github.com>
2022-06-17 18:43:41 -0600

    import project
    Imported the unity project, from base. Here ya go

---
+++ Project Lucidity Unity/Assets/Scripts/Player.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [Header("Physics Variables")]
+    public float acceleration;
+    public float maxSpeed;
+    public float friction;
+    public float jumpForce;
+    public float fallMultiplier;
+
+    private bool onGround = false;
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start(){
+        rb = gameObject.GetComponent<Rigidbody2D>(); // stores player's rigidbody (physics)
+    }
+
+    // Update is called once per frame
+    void Update(){
+        if(Input.GetAxisRaw("Horizontal") != 0){
+            rb.velocity += new Vector2(Input.GetAxisRaw("Horizontal") * acceleration, 0); // accelerate
+            rb.velocity = new Vector2(Mathf.Clamp(rb.velocity.x, -maxSpeed, maxSpeed), rb.velocity.y); // caps speed
+        }else{
+            rb.velocity = new Vector2(rb.velocity.x / friction, rb.velocity.y); // slows down faster
+        }
+        if(Input.GetButtonDown("Jump")){ // if touching ground and jump key pressed
+            rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse); // jump
+        }
+        if(rb.velocity.y < 0){
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * fallMultiplier); // makes heavier falling
+        }
+    }
+}
