8bc1e5416745b5dc31625016fd6d6f252f1c767c
ImYellowToad <104537507+ImYellowToad@users.noreply.github.com>
2022-08-05 15:18:55 -0600

    hi

---
+++ Project Lucidity Unity/Assets/WeaponPlaceholder.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponPlaceholder : MonoBehaviour
+{
+    public Transform FirePointA;
+    public Transform FirePointB;
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
